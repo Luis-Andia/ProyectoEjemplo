827156eabae4accfe5b6c88f6b1a486307eb7d66
Luis_AS <u202417048@upc.edu.pe>
2026-08-29 11:35:32 -0500

    comentarios

--- main.c
+++ main.c
@@ -1,4 +1,9 @@
 int main (void)
 {
     printf("Hola Mundo");
+    // git clone <repo> [Para clonar un repositorio]
+    // git fetch [Para descargar la informacion del clon]
+    // git pull origin main [Extraer las actualizaciones de la nube]
+    // git commit -am "mensaje" [Para guardar cambios locales]
+    // git push origin main  [Para guardar cambios en la nube]
 }
